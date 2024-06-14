0d20a1df5976d8676b7a73ff06f7a14344215480
JimPaulClarianes <jimpaulbatutayclarianes@gmail.com>
2024-06-14 11:01:07 +0800

    added file

---
+++ index.js
@@ -0,0 +1,21 @@
+// let formSection = document.querySelector(".form-section");
+let formContaioner = document.querySelector('.form-container');
+let exitContainer = document.querySelector('#exit-container');
+let joinBtn = document.querySelector("#join-btn");
+let exitBtn = document.querySelector("#exit-btn");
+
+exitContainer.addEventListener('click', () => {
+    formContaioner.style.display = 'none';
+});
+
+joinBtn.addEventListener('click', () => {
+    formContaioner.style.display = 'block';
+    // formSection.style.display = 'block';
+});
+
+exitBtn.addEventListener('click', () => {
+    formContaioner.style.display = 'none';
+    // formSection.style.display = 'none';
+});
+
+
